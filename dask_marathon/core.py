d41ce9e232a752045ddeb213be4c91ef73f0e7e7
Matthew Rocklin <mrocklin@gmail.com>
2016-09-19 17:08:29 -0400

    Remove dead code

--- dask_marathon/core.py
+++ dask_marathon/core.py
@@ -14,30 +14,30 @@ logger = logging.getLogger(__file__)
 
 class AdaptiveCluster(Adaptive):
     def __init__(self, scheduler, executable='dask-worker',
-            marathon_address='http://localhost:8080', name=None,
-            minimum_instances=0, **kwargs):
-
-        self.client = MarathonClient(marathon_address)
+            marathon_address='http://localhost:8080', name=None, **kwargs):
         self.scheduler = scheduler
-        self.minimum_instances = minimum_instances
-        self.marathon_address = marathon_address
         self.executor = ThreadPoolExecutor(1)
 
+        # Create Marathon App to run dask-worker
         args = [executable, scheduler.address,
                 '--name', '$MESOS_TASK_ID']
         if 'mem' in kwargs:
             args.extend(['--memory-limit',
                          str(int(kwargs['mem'] * 0.6 * 1e6))])
         kwargs['cmd'] = ' '.join(args)
+        app = MarathonApp(instances=0, **kwargs)
 
-        app = MarathonApp(instances=minimum_instances, **kwargs)
+        # Connect and register app
+        self.client = MarathonClient(marathon_address)
         self.app = self.client.create_app(name or 'dask-%s' % uuid.uuid4(), app)
+
         super(AdaptiveCluster, self).__init__()
 
     @gen.coroutine
     def scale_up(self, instances):
         instances = max(1, len(self.scheduler.ncores) * 2)
-        self.client.scale_app(self.app.id, instances=instances)
+        yield self.executor.submit(self.client.scale_app,
+                self.app.id, instances=instances)
 
     @gen.coroutine
     def scale_down(self, workers):
