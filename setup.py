a68e057cfaecaa0737d7222ef5288ee719cc8fbe
Szucs Krisztian <szucs.krisztian@gmail.com>
2017-02-03 08:21:07 +0100

    removed dockerfiles; updated setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ from setuptools import setup
 setup(name='dask_marathon',
       version='0.0.1',
       description='Deploy Dask on Marathon',
-      url='http://github.com/mrocklin/dask-marathon/',
+      url='http://github.com/dask/dask-marathon/',
       maintainer='Matthew Rocklin',
       maintainer_email='mrocklin@gmail.com',
       license='BSD',
       keywords='',
       packages=['dask_marathon'],
-      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      tests_require=['pytest'],
+      setup_requires=['pytest-runner'],
+      install_requires=['distributed', 'marathon==0.8.9'],
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       zip_safe=False)
